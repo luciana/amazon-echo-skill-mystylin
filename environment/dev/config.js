8df0a2aeeac2e08791381c07cc0592cc98e0d1ae
Luciana Bruscino <luciana.bruscino@gmail.com>
2017-09-05 08:25:03 -0400

    removed env folder from github

--- environment/dev/config.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-
-config.app_id = 'amzn1.ask.skill.4e2a1c40-da3e-46be-8af0-7299315d43b4';
-config.api_key = 'AIzaSyCQQWWc5j1hxmuJoLlMThUFxlbcMo3yGxA';
-config.host_name = 'mystylinapi.azurewebsites.net';
-config.host = 'https://mystylinapi.azurewebsites.net';
-module.exports = config;
--- environment/prod/config.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-
-config.app_id = 'amzn1.ask.skill.db03e18f-2bc3-40d0-be69-19b2cfff3276';
-config.api_key = 'AIzaSyCQQWWc5j1hxmuJoLlMThUFxlbcMo3yGxA';
-config.host_name = 'mystylinapi.azurewebsites.net';
-config.host = 'https://mystylinapi.azurewebsites.net';
-module.exports = config;
